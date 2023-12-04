af8b271d26b7b244d166a90aeaae63c3371218f4
mincheol <mincheol@douzone.com>
2023-12-04 21:48:57 +0900

    health check router 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [CommonModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
