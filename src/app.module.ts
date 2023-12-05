220fe765be03b977e80ab085f71eddb065f4d899
mincheol <mincheol@douzone.com>
2023-12-05 21:34:22 +0900

    [feat] 환경변수 셋업

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,38 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-	imports: [CommonModule],
+	imports: [
+		CommonModule,
+		ConfigModule.forRoot({
+			isGlobal: true,
+			ignoreEnvFile: process.env.NODE_ENV === 'prod',
+			validationSchema: Joi.object({
+				NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+				DB_HOST: Joi.string().required(),
+				DB_PORT: Joi.string().required(),
+				DB_USERNAME: Joi.string().required(),
+				DB_PASSWORD: Joi.string().required(),
+				DB_NAME: Joi.string().required(),
+			}),
+			envFilePath: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev',
+		}),
+		TypeOrmModule.forRoot({
+			type: 'mysql',
+			host: process.env.DB_HOST,
+			port: +process.env.DB_PORT,
+			username: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME,
+			synchronize: true,
+			logging: true,
+			entities: [],
+		}),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
